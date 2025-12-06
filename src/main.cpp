5a64bfc17c108b0c3535a510780a0f1913c06989
Lillie <75392499+iLillie@users.noreply.github.com>
2025-12-06 13:52:13 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "settings.hpp"
 #include "basic-string.hpp"
 #include "shader-patch.hpp"
-#include "type-defines.hpp"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "beatsaber-hook/shared/utils/typedefs.h"
@@ -10,43 +9,8 @@
 #include "modInfo.hpp"
 #include "scotland2/shared/modloader.h"
 
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_STATIC(MainEffectCore_SetGlobalShaderValues, "", "MainEffectCore", "SetGlobalShaderValues", void, float baseColorBoost, float baseColorBoostThreshold) {
-    //baseColorBoost = 0.15f;
-    //baseColorBoostThreshold = 0.0f;
-    MainEffectCore_SetGlobalShaderValues(baseColorBoost, baseColorBoostThreshold);
-}
-
-
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(MainEffectController_OnPreRender, "", "MainEffectController", "OnPreRender", void, Il2CppObject* self) {
-   //auto shader = il2cpp_utils::GetClassFromName("UnityEngine", "Shader");
-    auto imageEffectController = *il2cpp_utils::GetFieldValue<ImageEffectController*>(self, "_imageEffectController");
-    auto _mainEffectContainer = *il2cpp_utils::GetFieldValue<MainEffectContainerSO*>(self, "_mainEffectContainer");
-    auto mainEffect = *il2cpp_utils::GetFieldValue<MainEffectSO*>(_mainEffectContainer, "_mainEffect");
-    il2cpp_utils::RunMethod<void>(mainEffect, "PreRender");
-    il2cpp_utils::SetPropertyValue(imageEffectController, "enabled", true);
-    auto value = SystemBool{true};
-
-    il2cpp_utils::RunMethod<void>(reinterpret_cast<MainEffectCore*>(il2cpp_utils::GetClassFromName("", "MainEffectCore")), "UpdateKeyword", value);
-}
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(SettingsApplicatorSO_ApplyGraphicSettings, "", "SettingsApplicatorSO", "ApplyGraphicSettings", void, Il2CppObject* self, Settings settings, int other) {
-    settings.quality.mainEffect = QualitySettings::Game_;
-    settings.quality.bloom = QualitySettings::Game;
-    settings.quality.obstacles = QualitySettings::High_;
-    settings.quality.smokeGraphics = true;
-    settings.quality.screenDisplacementEffects = true;
-    settings.quality.maxShockwaveParticles = 2;
-    settings.quality.burnMarkTrails = true;
-    settings.quality.depthTexture = true;
-    settings.quality.mirror = QualitySettings::High;
-    settings.quality.antiAliasingLevel = 0;
-    SettingsApplicatorSO_ApplyGraphicSettings(self, settings, other);
-}
-
 MAKE_HOOK_FIND_CLASS_UNSAFE_STATIC(SettingValidations_AdjustQuest3, "BeatSaber.Settings", "SettingValidations", "AdjustQuest3", void, Settings* settings) {
-    //SettingValidations_AdjustQuest3(settings);
+    SettingValidations_AdjustQuest3(settings);
     settings->quality.mainEffect = QualitySettings::Game_;
     settings->quality.bloom = QualitySettings::Game;
     settings->quality.obstacles = QualitySettings::High_;
@@ -57,66 +21,6 @@ MAKE_HOOK_FIND_CLASS_UNSAFE_STATIC(SettingValidations_AdjustQuest3, "BeatSaber.S
     settings->quality.depthTexture = true;
     settings->quality.mirror = QualitySettings::High;
     settings->quality.antiAliasingLevel = 0;
-}
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(GraphicSettingsConditionalActivator_GetGraphicsActivatorType, "", "GraphicSettingsConditionalActivator", "Start", bool, Il2CppObject* self) {
-    Logger.info("GraphicsSettingsConditional Start");
-    auto gameObject = *il2cpp_utils::RunMethod<GameObject*>(self, "get_gameObject").as_optional_result().value_or(nullptr);
-    auto name = *il2cpp_utils::GetPropertyValue<String*>(gameObject, "name").as_optional_result().value_or(nullptr);
-    auto name2 = il2cpp_utils::detail::to_string(static_cast<StringW>(name));
-    Logger.info("GraphicsSettingsConditional END: {}", name2);
-
-    return true;
-}
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(ObstacleController_InitGraphics, "", "ObstacleController", "InitGraphics", void, Il2CppObject* self, Settings settings) {
-    settings.quality.screenDisplacementEffects = true;
-    ObstacleController_InitGraphics(self, settings);
-}
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(ConditionalMaterialSwitcher_Awake, "", "ConditionalMaterialSwitcher", "Awake", void, Il2CppObject* self) {
-    auto gameObject = *il2cpp_utils::RunMethod<GameObject*>(self, "get_gameObject").as_optional_result().value_or(nullptr);
-
-    if (gameObject == nullptr) {
-        Logger.info("Nullptr found!");
-        ConditionalMaterialSwitcher_Awake(self);
-        return;
-    }
-
-    auto name = *il2cpp_utils::GetPropertyValue<String*>(gameObject, "name").as_optional_result().value_or(nullptr);
-
-    if (name == nullptr) {
-        Logger.info("no name string found!");
-        ConditionalMaterialSwitcher_Awake(self);
-        return;
-    }
-
-    auto valueBool = *il2cpp_utils::GetFieldValue<BoolSO*>(self, "_value").value_or(nullptr);
-    auto value = *il2cpp_utils::GetPropertyValue<SystemBool, false>(valueBool, "value").as_optional_result().value_or(nullptr);
-
-    auto name2 = il2cpp_utils::detail::to_string(static_cast<StringW>(name));
-
-    if (name2 == "NoteCube" || name2 == "NoteArrow" || name2 == "SaberGlowingEdges" || name2 == "ObstacleFrame" || name2 == "SaberTrailRendererHologram(Clone)")  {
-        value.m_value = true;
-        il2cpp_utils::SetPropertyValue<false>(valueBool, "value", value);
-    }
-
-    ConditionalMaterialSwitcher_Awake(self);
-}
-
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(ConditionalActivation_Awake, "", "ConditionalActivation", "Awake", void, Il2CppObject* self) {
-    auto gameObject = *il2cpp_utils::GetPropertyValue<GameObject*>(self, "gameObject").as_optional_result().value_or(nullptr);
-    auto name = *il2cpp_utils::GetPropertyValue<String*>(gameObject, "name").as_optional_result().value_or(nullptr);
-    auto valueBool = *il2cpp_utils::GetFieldValue<BoolSO*>(self, "_value").value_or(nullptr);
-    auto value = *il2cpp_utils::GetPropertyValue<SystemBool>(valueBool, "value").as_optional_result().value_or(nullptr);
-    auto name2 = il2cpp_utils::detail::to_string(static_cast<StringW>(name));
-
-    if ( name2 == "ObstacleFrame")  {
-        value.m_value = true;
-        il2cpp_utils::SetPropertyValue(valueBool, "value", value);
-    }
-
-    ConditionalActivation_Awake(self);
 }
 
 MAKE_HOOK(hCompileProgramImpl, nullptr, void,
@@ -130,9 +34,8 @@ MAKE_HOOK(hCompileProgramImpl, nullptr, void,
     int& shader, int a, int * b, int *c, int* d) {
     auto shaderName_c = shaderName.c_str();
 
-
     // TODO: Figure out how to fix for mirror?
-    if (strcmp(shaderName_c, "Custom/ParametricBoxFrameHD") == 0 && fragment.heap.size > 5390) {
+    if (strcmp(shaderName_c, "Custom/ParametricBoxFrameHD") == 0 && fragment.heap.size > 5400 && fragment.heap.size < 5500) {
         memcpy((void*)fragment.heap.data, (void*)&ParametricBoxFrameHD_fix_glsl, ParametricBoxFrameHD_fix_glsl_len);
         fragment.heap.size = ParametricBoxFrameHD_fix_glsl_len-1;
     }
@@ -167,7 +70,6 @@ MOD_EXPORT_FUNC void load() {
         il2cpp_functions::Init();
         return;
     }
-
     void* jniOnUnload = dlsym(modloader_unity_handle, "JNI_OnUnload");
 
     // GlslGpuProgramGLES::CompileProgramImpl
@@ -180,12 +82,5 @@ MOD_EXPORT_FUNC void load() {
 /// @brief Called later on in the game loading - a good time to install function hooks
 /// @return
 MOD_EXPORT_FUNC void late_load() {
-    INSTALL_HOOK(Logger, GraphicSettingsConditionalActivator_GetGraphicsActivatorType);
     INSTALL_HOOK(Logger, SettingValidations_AdjustQuest3);
-    //INSTALL_HOOK(Logger, ConditionalMaterialSwitcher_Awake);
-    //INSTALL_HOOK(Logger, ConditionalActivation_Awake);
-    INSTALL_HOOK(Logger, SettingsApplicatorSO_ApplyGraphicSettings);
-    INSTALL_HOOK(Logger, ObstacleController_InitGraphics);
-    //INSTALL_HOOK(Logger, MainEffectController_OnPreRender);
-    INSTALL_HOOK(Logger, MainEffectCore_SetGlobalShaderValues);
 }
